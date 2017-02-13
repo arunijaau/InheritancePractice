7b3143a7a2a500c3dced48bd09eb075015887a55
Aruni <Aruni@maxxi2.attlocal.net>
2017-02-13 00:10:55 -0600

    Add Imaginary objects.

---
+++ src/inheritancepractice/AngryBird.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritancepractice;
+
+/**
+ *
+ * @author Aruni
+ */
+public class AngryBird {
+    private String name;
+    
+    
+}
